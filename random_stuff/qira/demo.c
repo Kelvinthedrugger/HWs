66d79acd2dd953ca1518e4f9fab62ce8e697a7f0
Kelvinthedrugger <890225kelvin@gmail.com>
2022-06-27 00:34:20 +0800

    add qira eg

---
+++ random_stuff/qira/demo.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main()
+{
+    volatile int a = 54, b = 12, c = 88;
+
+    int i;
+    for (i = 0; i < 5; ++i_
+    {
+        c += b + i;
+    }
+
+    int guess;
+    printf("number: "); fflush(stdout);
+    scanf("%d", &guess);
+
+    int ans = a * b + 4 * c;
+    if (ans == guess)
+    {
+        printf("WINNER!\n");
+    }
+    else
+    {
+        printf("NOPE :(\n");
+    }
+    return 0;
+}
